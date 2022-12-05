93bc6bca01e54ccf8212d1bf9db446cc591f891f
Jagan <62688141+jagan510710@users.noreply.github.com>
2022-12-05 10:25:42 +0530

    Add files via upload

---
+++ SampleWindow/SampleWindow/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SampleWindow
+//
+//  Created by C2019511 on 21/09/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
